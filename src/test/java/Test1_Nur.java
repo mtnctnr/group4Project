59b6387bb8e0f4cf66009a2327bcd1ca40ec2eba
Nur <feyzanursahinli@gmail.com>
2022-04-27 22:16:59 +0200

    Nur committing 2

--- src/test/java/Test1_Nur.java
+++ src/test/java/Test1_Nur.java
@@ -1,6 +1,7 @@
 public class Test1_Nur {
     public static void main(String[] args) {
         System.out.println("Hello from Nur");
+        System.out.println("This is second commit");
 
     }
 }
