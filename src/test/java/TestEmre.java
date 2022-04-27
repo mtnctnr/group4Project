3b329308d62f80d7d77f9bf9e6cbcf5299cf58a1
emreavaz <emreavazzz.gmail.com>
2022-04-27 22:19:57 +0200

    commit 3

--- src/test/java/TestEmre.java
+++ src/test/java/TestEmre.java
@@ -3,5 +3,6 @@ public class TestEmre {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Commit 2");
+        //group project
     }
 }
