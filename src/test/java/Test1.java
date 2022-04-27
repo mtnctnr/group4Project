88f23580f01755a20d48b38ff2c8894964aee815
cholponai <cholpondzhumabaeva@gmail.com>
2022-04-27 22:18:59 +0200

    Commit3

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -2,5 +2,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello Cydeo");
         System.out.println("Hello Git");
+        System.out.println("Wooden Spoon");
     }
 }
